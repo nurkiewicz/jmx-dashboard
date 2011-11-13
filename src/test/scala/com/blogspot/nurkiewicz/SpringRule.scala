c0311e25cbbbe9e68d8f6be35aa6e523bd950ee0
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-11-13 23:53:46 +0100

    Cosmetics, formatting

--- src/test/scala/com/blogspot/nurkiewicz/SpringRule.scala
+++ src/test/scala/com/blogspot/nurkiewicz/SpringRule.scala
@@ -11,8 +11,7 @@ import org.springframework.beans.factory.BeanFactory
  * @author Tomasz Nurkiewicz
  * @since 08.10.11, 00:06
  */
-
-trait SpringRule extends Suite with BeforeAndAfterAll {
+trait SpringRule extends Suite with BeforeAndAfterAll { this: AbstractSuite =>
 
 	@Autowired val beanFactory: BeanFactory = null
 
