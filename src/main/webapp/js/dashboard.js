01cf27793edd0ac5bb661b74185e5c14fe187bc1
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-12-17 19:54:48 +0100

    Smoke test of jsTree

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -28,4 +28,42 @@ $(function() {
             }
             }
     );
+
+	$("#jmxTree").jstree({
+		"json_data" : {
+			"data" : [
+				{
+					"data" : {title: "A node", icon: 'img/accept.png'},
+					"metadata" : { id : 23 },
+					"children" : [
+						{
+							"data" : {title: "B node", icon: 'img/magnifier.png'},
+							"children" : [ "Child 1", "A Child 2" ]
+						},
+						{
+							"data" : {title: "C node", icon: 'img/error.png'},
+							"children" : [ "Child 1", "A Child 2" ]
+						},
+						{
+							"data" : {title: "D node", icon: 'img/alarm_bell.png'},
+							"children" : [ "Child 1", "A Child 2" ]
+						}
+
+					]
+				},
+				{
+					"attr" : { "id" : "li.node.id1" },
+					"data" : {
+						"title" : "Long format demo",
+						"attr" : { "href" : "#" }
+					}
+				}
+			]
+		},
+		"core" : { "animation" : 250 },
+		"plugins" : ["json_data", "ui", "themeroller"]
+	}).bind("select_node.jstree", function (e, data) {
+				data.inst.toggle_node(data.rslt.obj);
+			});
+
 });
