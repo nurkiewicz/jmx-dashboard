dd205798562ecedbb321dda4b198bfb3e3876456
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-12-18 22:00:20 +0100

    Formatting

--- src/main/scala/com/blogspot/nurkiewicz/spring/JmxLocalContainerEntityManagerFactoryBean.scala
+++ src/main/scala/com/blogspot/nurkiewicz/spring/JmxLocalContainerEntityManagerFactoryBean.scala
@@ -6,24 +6,25 @@ import javax.persistence.EntityManagerFactory
 import org.hibernate.ejb.EntityManagerFactoryImpl
 import javax.management.ObjectName
 import management.ManagementFactory
-;
+
 
 class JmxLocalContainerEntityManagerFactoryBean() extends LocalContainerEntityManagerFactoryBean {
-    override def createNativeEntityManagerFactory() = {
-        val managerFactory = super.createNativeEntityManagerFactory()
-        registerStatisticsMBean(managerFactory)
-        managerFactory
-    }
+	override def createNativeEntityManagerFactory() = {
+		val managerFactory = super.createNativeEntityManagerFactory()
+		registerStatisticsMBean(managerFactory)
+		managerFactory
+	}
 
-    def registerStatisticsMBean(managerFactory: EntityManagerFactory) {
-	    managerFactory match {
-		    case impl: EntityManagerFactoryImpl =>
-			    val mBean = new StatisticsService();
-			    mBean.setSessionFactory(impl.getSessionFactory);
-			    val name = new ObjectName("org.hibernate:type=Statistics,application=spring-pitfalls")
-			    ManagementFactory.getPlatformMBeanServer.registerMBean(mBean, name);
-		    case _ =>
-	    }
-    }
+	def registerStatisticsMBean(managerFactory: EntityManagerFactory) {
+		managerFactory match {
+			case impl: EntityManagerFactoryImpl =>
+				val mBean = new StatisticsService();
+				mBean.setStatisticsEnabled(true)
+				mBean.setSessionFactory(impl.getSessionFactory);
+				val name = new ObjectName("org.hibernate:type=Statistics,application=spring-pitfalls")
+				ManagementFactory.getPlatformMBeanServer.registerMBean(mBean, name);
+			case _ =>
+		}
+	}
 
 }
