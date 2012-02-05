c6298601e1579a7e5aac679236c336f6bf7883f3
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2012-02-05 10:57:59 +0100

    Selected JMX metrics displayed in groups with (sort of) reasonable thresholds

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -11,14 +11,15 @@ $(function() {
 			"java.lang:type=OperatingSystem",
 			"java.lang:type=Runtime",
 			"java.lang:type=Threading",
-			"com.blogspot.nurkiewicz.spring:name=dataSource,type=ManagedBasicDataSource",
-			"quartz:type=QuartzScheduler,name=schedulerFactory,instance=NON_CLUSTERED",
-			"net.sf.ehcache:type=CacheStatistics,CacheManager=spring-pitfalls,name=org.hibernate.cache.StandardQueryCache",
-			"net.sf.ehcache:type=CacheStatistics,CacheManager=spring-pitfalls,name=org.hibernate.cache.UpdateTimestampsCache",
 			'Catalina:name="http-bio-8080",type=ThreadPool',
 			'Catalina:type=GlobalRequestProcessor,name="http-bio-8080"',
 			'Catalina:type=Manager,context=/spring-pitfalls,host=localhost',
-			'org.hibernate:type=Statistics,application=spring-pitfalls'
+			'org.hibernate:type=Statistics,application=spring-pitfalls',
+			"net.sf.ehcache:type=CacheStatistics,CacheManager=spring-pitfalls,name=org.hibernate.cache.StandardQueryCache",
+			"net.sf.ehcache:type=CacheStatistics,CacheManager=spring-pitfalls,name=org.hibernate.cache.UpdateTimestampsCache",
+			"quartz:type=QuartzScheduler,name=schedulerFactory,instance=NON_CLUSTERED",
+			'org.apache.activemq:BrokerName=localhost,Type=Queue,Destination=requests',
+			"com.blogspot.nurkiewicz.spring:name=dataSource,type=ManagedBasicDataSource"
 		];
 		return _.map(mbeans, function(mbean) {
 			return {
@@ -28,32 +29,39 @@ $(function() {
 		});
 	}
 
-	function jmx(response) {
+	function jmxAttributes(response) {
 		var jmxMap = {};
 		_.each(response, function(mbean) {
-			jmxMap[mbean.request.mbean] = mbean.value;
+			if(mbean.status == 200) {
+				jmxMap[mbean.request.mbean] = mbean.value;
+			} else {
+				throw new Error(mbean.error)
+			}
 		});
 		return jmxMap;
+	}
+
+	function buildTree(model) {
+		$("#jmxTree").jstree({
+			"json_data" : { "data" : model},
+			"core" : { "animation" : 250 },
+			"plugins" : ["json_data", "ui", "themeroller"]
+		}).bind("select_node.jstree", function (e, data) {
+					data.inst.toggle_node(data.rslt.obj);
+					console.info(data.rslt.obj.data('metric'));
+				});
 	}
 
 	$.ajax({
 		url: 'jmx?ignoreErrors=true',
 		type: "POST",
 		dataType: "json",
-		data: JSON.stringify(request),
+		data: JSON.stringify(request()),
 		contentType: "application/json",
 		success: function(response) {
-			var model = buildTreeModel(response);
+			var model = buildTreeModel(jmxAttributes(response));
 			var jsTree = treeModelToJsTree(model);
-
-			$("#jmxTree").jstree({
-				"json_data" : { "data" : jsTree },
-				"core" : { "animation" : 250 },
-				"plugins" : ["json_data", "ui", "themeroller"]
-			}).bind("select_node.jstree", function (e, data) {
-						data.inst.toggle_node(data.rslt.obj);
-						console.info(data.rslt.obj.data('metric'));
-					});
+			buildTree(jsTree);
 		}
 	});
 
@@ -69,20 +77,23 @@ $(function() {
 	Node.threshold = function(attention, warning, error) {
 			if(attention > warning && warning > error) {
 				return function(value) {
-					if(value >= attention) { return 1.0; }
-					if(value >= warning) { return 0.5; }
-					if(value >= error) { return 0.0; } else { return -1.0; }
+					if(value > attention) { return 1.0; }
+					if(value > warning) { return 0.5; }
+					if(value > error) { return 0.0; } else { return -1.0; }
 				}
 			}
 			if(attention < warning && warning < error) {
 				return function(value) {
-					if(value <= attention) { return 1.0; }
-					if(value <= warning) { return 0.5; }
-					if(value <= error) { return 0.0; } else { return -1.0; }
+					if(value < attention) { return 1.0; }
+					if(value < warning) { return 0.5; }
+					if(value < error) { return 0.0; } else { return -1.0; }
 				}
 			}
-			throw new Error("All thresholds should either be increasing or decreasing");
+			throw new Error("All thresholds should either be increasing or decreasing: " + attention + ", " + warning + ", " + error);
 		};
+	Node.relativeThreshold = function(attention, warning, error, max) {
+		return Node.threshold(attention * max, warning * max, error * max);
+	};
 
 	function CompositeNode(label, children) {
 		Node.call(this, label, undefined, function() {
@@ -96,27 +107,100 @@ $(function() {
 	CompositeNode.prototype.constructor = CompositeNode;
 
 	function buildTreeModel(jmx) {
-		return new CompositeNode('General', [
-			new CompositeNode('Server', [
-				new CompositeNode('Hardware', [
-					new Node('Open file descriptor count', Math.random() * 25, Node.threshold(10, 18, 22)),
-					new Node('System load average', Math.random() * 25, Node.threshold(10, 18, 22)),
-					new CompositeNode('Memory', [
-						new Node('Free swap space size', Math.random() * 25, Node.threshold(10, 18, 22)),
-						new Node('Free physical memory size', Math.random() * 25, Node.threshold(10, 18, 22))
-					])
+		console.info(jmx);
+		return new CompositeNode('Overall', [
+			new CompositeNode('Servlet container', [
+				new Node(
+						'Active HTTP sessions',
+						jmx['Catalina:context=/spring-pitfalls,host=localhost,type=Manager'].activeSessions,
+						Node.threshold(200, 300, 500)
+				),
+				new Node(
+						'HTTP sessions create rate',
+						jmx['Catalina:context=/spring-pitfalls,host=localhost,type=Manager'].sessionCreateRate,
+						Node.threshold(5, 10, 50)
+				),
+				new Node(
+						'Rejected HTTP sessions',
+						jmx['Catalina:context=/spring-pitfalls,host=localhost,type=Manager'].rejectedSessions,
+						Node.threshold(1, 5, 10)
+				),
+				new Node(
+						'Busy worker threads count',
+						jmx['Catalina:name="http-bio-8080",type=ThreadPool'].currentThreadsBusy,
+						Node.relativeThreshold(0.85, 0.9, 0.95, jmx['Catalina:name="http-bio-8080",type=ThreadPool'].maxThreads)
+				),
+			]),
+			new CompositeNode('JVM', [
+				new Node(
+						'Code cache memory usage',
+						jmx['java.lang:name=Code Cache,type=MemoryPool'].Usage.used,
+						Node.relativeThreshold(0.75, 0.85, 0.95, jmx['java.lang:name=Code Cache,type=MemoryPool'].Usage.max)
+				),
+				new Node(
+						'Perm Gen memory usage',
+						jmx['java.lang:name=PS Perm Gen,type=MemoryPool'].Usage.used,
+						Node.relativeThreshold(0.75, 0.85, 0.95, jmx['java.lang:name=PS Perm Gen,type=MemoryPool'].Usage.max)
+				),
+				new Node(
+						'Heap memory usage',
+						jmx['java.lang:type=Memory'].HeapMemoryUsage.used,
+						Node.relativeThreshold(0.75, 0.85, 0.95, jmx['java.lang:type=Memory'].HeapMemoryUsage.max)
+				),
+				new Node(
+						'Non-heap memory usage',
+						jmx['java.lang:type=Memory'].NonHeapMemoryUsage.used,
+						Node.relativeThreshold(0.75, 0.85, 0.95, jmx['java.lang:type=Memory'].NonHeapMemoryUsage.max)
+				),
+				new Node(
+						'Total active threads',
+						jmx['java.lang:type=Threading'].ThreadCount,
+						Node.threshold(50, 75, 100)
+				)
+			]),
+			new CompositeNode('Operating system', [
+				new Node(
+						'Free physical memory',
+						jmx['java.lang:type=OperatingSystem'].FreePhysicalMemorySize,
+						Node.relativeThreshold(0.75, 0.85, 0.95, jmx['java.lang:type=OperatingSystem'].TotalPhysicalMemorySize)
+				),
+				new Node(
+						'Free swap space',
+						jmx['java.lang:type=OperatingSystem'].FreeSwapSpaceSize,
+						Node.relativeThreshold(0.75, 0.85, 0.95, jmx['java.lang:type=OperatingSystem'].TotalSwapSpaceSize)
+				),
+				new Node(
+						'Average system load (%)',
+						jmx['java.lang:type=OperatingSystem'].SystemLoadAverage,
+						Node.threshold(0.75, 0.85, 0.95)
+				),
+				new Node(
+						'Open file descriptors',
+						jmx['java.lang:type=OperatingSystem'].OpenFileDescriptorCount,
+						Node.threshold(100, 150, 200)
+				)
+			]),
+			new CompositeNode('External systems', [
+				new CompositeNode('Persistence', [
+					new Node(
+							'Active database connections',
+							jmx['com.blogspot.nurkiewicz.spring:name=dataSource,type=ManagedBasicDataSource'].NumActive,
+							Node.relativeThreshold(0.75, 0.85, 0.95, jmx['com.blogspot.nurkiewicz.spring:name=dataSource,type=ManagedBasicDataSource'].MaxActive)
+					),
 				]),
-				new CompositeNode('Operating system', []),
-				new CompositeNode('Application', [
-					new CompositeNode('Java virtual machine', []),
-					new CompositeNode('Persistence', [])
-				])
-			]),
-			new CompositeNode('Environment', [
-				new CompositeNode('External systems', []),
-				new Node('Web', Math.random() * 25, Node.threshold(10, 18, 22))
-			]),
-			new Node('Persistence', Math.random() * 25, Node.threshold(10, 18, 22)),
+				new CompositeNode('JMS messaging broker', [
+					new Node(
+							'Waiting in "requests" queue',
+							jmx['org.apache.activemq:BrokerName=localhost,Destination=requests,Type=Queue'].QueueSize,
+							Node.threshold(2, 5, 10)
+					),
+					new Node(
+							'Number of consumers',
+							jmx['org.apache.activemq:BrokerName=localhost,Destination=requests,Type=Queue'].ConsumerCount,
+							Node.threshold(0.2, 0.1, 0)
+					)
+				]),
+			])
 		]);
 	}
 
